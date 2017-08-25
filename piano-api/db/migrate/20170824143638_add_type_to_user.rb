6d22e40e1b18ef962b5228f46d4999a924d4eed5
jamesrhee91 <jamesshrhee@gmail.com>
2017-08-25 14:03:38 -0400

    donzo

--- piano-api/db/migrate/20170824143638_add_type_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddTypeToUser < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :mode, :string
-  end
-end
--- piano-api/db/schema.rb
+++ piano-api/db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170824143638) do
+ActiveRecord::Schema.define(version: 20170822215048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "score"
+    t.float "score"
+    t.string "mode"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "mode"
   end
 
 end
