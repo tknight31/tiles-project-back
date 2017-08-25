289447ad106615f0926f6e28c938181ece675996
jamesrhee91 <jamesshrhee@gmail.com>
2017-08-25 09:47:42 -0400

    arcade mode working

--- piano-api/db/schema.rb
+++ piano-api/db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20170824143638) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.float "score"
+    t.string "score"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "mode"
